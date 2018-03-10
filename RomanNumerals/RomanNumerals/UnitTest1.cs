e5500aadbc934cde9bf1251924c3e0b286f6e8ee
prachi <prachimuley.04@gmail.com>
2018-03-10 13:50:20 -0800

    Initial commit

---
+++ RomanNumerals/RomanNumerals/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RomanNumerals rmn = new RomanNumerals();
+            int number = rmn.romanToDecimal("MMXVI");
+            Assert.AreEqual(2016,number);
+        }
+    }
+}
