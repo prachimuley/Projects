eefcd1de698eceec7f27f756fe4a8cc77d75417c
prachi <prachimuley.04@gmail.com>
2018-03-10 22:27:34 -0800

    Fixed failing UT. Added new failing UT.
    All Roman Numeral sequences with decreasing value from left to right are now converted corrrectly.
    
    However, any Roman numeral sequence with at least 1 increasing value sequence will fail.

--- RomanNumerals/RomanNumerals/UnitTest1.cs
+++ RomanNumerals/RomanNumerals/UnitTest1.cs
@@ -6,12 +6,23 @@ namespace RomanNumerals
     [TestClass]
     public class UnitTest1
     {
+        // ToDo: Using Nunit this can actually be achieved in a single test method with multiple input attributes
         [TestMethod]
-        public void TestMethod1()
+        public void TestWithRomanNumberWithAllDecreasingValueSequence()
+        {
+            execRomanToDecimalConverter("CCCLXV");
+        }
+        [TestMethod]
+        public void TestWithRomanNumberWithAnIncreasingValueSequence()
+        {
+            execRomanToDecimalConverter("CCLXXXIX");
+        }
+
+    public void execRomanToDecimalConverter(String input)
         {
             RomanToDecimal rmn = new RomanToDecimal();
-            int number = rmn.converter("MMXVI");
-            Assert.AreEqual(2016,number);
+            int number = rmn.converter(input);
+            Assert.AreEqual(365, number);
         }
     }
 }
--- RomanNumerals/RomanToDecimal/Class1.cs
+++ RomanNumerals/RomanToDecimal/Class1.cs
@@ -8,9 +8,36 @@ namespace RomanToDecimalNameSpace
 {
     public class RomanToDecimal
     {
+        private Dictionary<char, int> map = new Dictionary<char, int>();
+        public RomanToDecimal()
+        {
+            initializeDictonary();
+        }
+
+        private void initializeDictonary()
+        {
+            map['I'] = 1;
+            map['V'] = 5;
+            map['X'] = 10;
+            map['L'] = 50;
+            map['C'] = 100;
+            map['D'] = 500;
+            map['M'] = 1000;
+        }
+        //This function will convert Roman Numerals to decimals for input value 1 to 4000.
         public int converter(String s)
         {
-            return 0;
+            int number = 0;
+
+            for (int i=0; i < s.Length-1; i++)
+            {
+                if ((map[s[i]]) >= map[s[i + 1]])
+                {
+                    number = map[s[i]] + number;
+                }
+            }
+            number = number + map[s[s.Length-1]];
+            return number;
         }
     }
 }
